7dd4156139e46aef6bd9cf305c43f96e82c28a70
Dmitry Blyukherov <d.blukherov@gmail.com>
2023-05-18 12:53:19 +0300

    Initial commit

---
+++ src/main/java/sky/pro/telegrambot/TelegrambotDemoApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegrambotDemoApplication.class, args);
+    }
+
+}
